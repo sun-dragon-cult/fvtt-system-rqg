250ade64b25b89e11f36037bd6606f982b1a21ca
Åke Wivänge <ake+wake@wivange.se>
2020-07-15 11:41:59 +0200

    Minor fixes

--- src/module/item/passion-sheet.ts
+++ src/module/item/passion-sheet.ts
@@ -2,7 +2,7 @@ import { passionType } from "../data-model/item-data/item-types";
 import { PassionsEnum } from "../data-model/item-data/passion";
 
 export class PassionSheet extends ItemSheet {
-  static get defaultOptions() {
+  static get defaultOptions(): FormApplicationOptions {
     return mergeObject(super.defaultOptions, {
       classes: ["rqg", "sheet", passionType],
       template: "systems/rqg/module/item/passion-sheet.html",
@@ -16,36 +16,4 @@ export class PassionSheet extends ItemSheet {
     data.data.passionTypes = Object.keys(PassionsEnum);
     return data;
   }
-
-  // /** @override */
-  // setPosition(options = {}) {
-  //   const position = super.setPosition(options);
-  //   const sheetBody = this.element.find(".sheet-body");
-  //   const bodyHeight = position.height - 150;
-  //   sheetBody.css("height", bodyHeight);
-  //   return position;
-  // }
-  //
-  // /** @override */
-  // activateListeners(html: JQuery) {
-  //   super.activateListeners(html);
-  //
-  //   // Everything below here is only needed if the sheet is editable
-  //   if (!this.options.editable) return;
-  // }
-  //
-  // /** @override */
-  // _updateObject(_: Event, formData: any) {
-  //   // Re-combine formData
-  //   formData = Object.entries(formData).reduce<any>(
-  //     (obj, e) => {
-  //       obj[e[0]] = e[1];
-  //       return obj;
-  //     },
-  //     { _id: this.object._id }
-  //   );
-  //
-  //   // Update the Item
-  //   return this.object.update(formData);
-  // }
 }
--- src/module/item/skill-sheet.ts
+++ src/module/item/skill-sheet.ts
@@ -2,7 +2,7 @@ import { skillType } from "../data-model/item-data/item-types";
 import { SkillCategoryEnum } from "../data-model/item-data/skill";
 
 export class SkillSheet extends ItemSheet {
-  static get defaultOptions() {
+  static get defaultOptions(): FormApplicationOptions {
     return mergeObject(super.defaultOptions, {
       classes: ["rqg", "sheet", skillType],
       template: "systems/rqg/module/item/skill-sheet.html",
@@ -17,36 +17,4 @@ export class SkillSheet extends ItemSheet {
     data.data.isGM = this.actor ? !this.actor.isPC : true;
     return data;
   }
-
-  // /** @override */
-  // setPosition(options = {}) {
-  //   const position = super.setPosition(options);
-  //   const sheetBody = this.element.find(".sheet-body");
-  //   const bodyHeight = position.height - 150;
-  //   sheetBody.css("height", bodyHeight);
-  //   return position;
-  // }
-  //
-  // /** @override */
-  // activateListeners(html: JQuery) {
-  //   super.activateListeners(html);
-  //
-  //   // Everything below here is only needed if the sheet is editable
-  //   if (!this.options.editable) return;
-  // }
-  //
-  // /** @override */
-  // _updateObject(_: Event, formData: any) {
-  //   // Re-combine formData
-  //   formData = Object.entries(formData).reduce<any>(
-  //     (obj, e) => {
-  //       obj[e[0]] = e[1];
-  //       return obj;
-  //     },
-  //     { _id: this.object._id }
-  //   );
-  //
-  //   // Update the Item
-  //   return this.object.update(formData);
-  // }
 }
