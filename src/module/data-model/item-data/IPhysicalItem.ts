6b228176ffb676c3c3eca163e2574d6372664bc0
Åke Wivänge <ake@wivange.se>
2020-07-29 23:48:02 +0200

    Add equipped attribute to physical items

--- src/module/data-model/item-data/IPhysicalItem.ts
+++ src/module/data-model/item-data/IPhysicalItem.ts
@@ -1,5 +1,5 @@
 export interface IPhysicalItem {
   quantity: number;
   encumbrance: number;
-  // equipped: boolean; // TODO This is not a property of the actual item - should it be a flag?
+  equipped: boolean;
 }
