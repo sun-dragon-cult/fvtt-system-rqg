90eabb141090d802dc383723e0e2f05e7691bd59
Åke Wivänge <ake.wivange@pagero.com>
2021-07-19 23:45:40 +0200

    fix: about time does not work the same anymore - switch to simple calendar

--- src/rqg.ts
+++ src/rqg.ts
@@ -47,11 +47,70 @@ Hooks.once("ready", async () => {
     } catch (err) {
       await game.settings.set("rqg", "runes", []);
     }
-
-    if (game.modules.get("about-time")?.active) {
-      const gameTime: any = game.GameTime;
-      gameTime.DTC.createFromData(gameTime.calendars.Glorantha);
-      gameTime.DTC.saveUserCalendar(gameTime.calendars.Glorantha);
-    }
+    await setupSimpleCalendar();
   }
 });
+
+async function setupSimpleCalendar() {
+  const simpleCalendarModule = "foundryvtt-simple-calendar";
+
+  if (game.modules.get(simpleCalendarModule)?.active) {
+    // that file is an export of simple-calendar settings
+    const calendar = await import("./module-integration/simple-calendar-glorantha.json");
+    const calendarSettings = (calendar as any).default;
+
+    console.log("RQG | Configuring simple calendar for Glorantha");
+
+    if (calendarSettings.hasOwnProperty("yearSettings")) {
+      await game.settings.set(simpleCalendarModule, "year-config", calendarSettings.yearSettings);
+    }
+    if (calendarSettings.hasOwnProperty("monthSettings")) {
+      await game.settings.set(simpleCalendarModule, "month-config", calendarSettings.monthSettings);
+    }
+    if (calendarSettings.hasOwnProperty("weekdaySettings")) {
+      await game.settings.set(
+        simpleCalendarModule,
+        "weekday-config",
+        calendarSettings.weekdaySettings
+      );
+    }
+    if (calendarSettings.hasOwnProperty("leapYearSettings")) {
+      await game.settings.set(
+        simpleCalendarModule,
+        "leap-year-rule",
+        calendarSettings.leapYearSettings
+      );
+    }
+    if (calendarSettings.hasOwnProperty("timeSettings")) {
+      await game.settings.set(
+        simpleCalendarModule,
+        "time-configuration",
+        calendarSettings.timeSettings
+      );
+    }
+    if (calendarSettings.hasOwnProperty("seasonSettings")) {
+      await game.settings.set(
+        simpleCalendarModule,
+        "season-configuration",
+        calendarSettings.seasonSettings
+      );
+    }
+    if (calendarSettings.hasOwnProperty("moonSettings")) {
+      await game.settings.set(
+        simpleCalendarModule,
+        "moon-configuration",
+        calendarSettings.moonSettings
+      );
+    }
+    if (calendarSettings.hasOwnProperty("generalSettings")) {
+      await game.settings.set(
+        simpleCalendarModule,
+        "general-configuration",
+        calendarSettings.generalSettings
+      );
+    }
+    // if (calendarSettings.hasOwnProperty("currentDate")) {
+    //   await game.settings.set(simpleCalendarModule, "current-date", calendarSettings.currentDate);
+    // }
+  }
+}
