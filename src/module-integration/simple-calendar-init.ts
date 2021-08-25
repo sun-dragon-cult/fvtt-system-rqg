9a93f0295953614bae735a29abbc389f66893acc
Åke Wivänge <ake.wivange@pagero.com>
2021-08-25 23:32:47 +0200

    fix: simple calendar init fails

--- src/module-integration/simple-calendar-init.ts
+++ src/module-integration/simple-calendar-init.ts
@@ -4,7 +4,7 @@ const simpleCalendarModule = "foundryvtt-simple-calendar";
 export async function setupSimpleCalendar() {
   if (game.modules.get(simpleCalendarModule)?.active) {
     const calendar = await import("./simple-calendar-glorantha.json"); // that file is an export of simple-calendar settings with current date removed
-    SimpleCalendar.api.configureCalendar(calendar);
+    SimpleCalendar.api.configureCalendar(calendar.default);
     return;
   }
 }
