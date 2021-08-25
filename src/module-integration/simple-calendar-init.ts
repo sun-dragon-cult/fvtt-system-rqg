9b8c5e77e31a6df2080464a3bf957a3ac3b9cf59
Åke Wivänge <ake.wivange@pagero.com>
2021-08-25 08:58:47 +0200

    chore: use simple-calendars new setup method instead of doing it myself

--- src/module-integration/simple-calendar-init.ts
+++ src/module-integration/simple-calendar-init.ts
@@ -1,63 +1,10 @@
+declare const SimpleCalendar: any;
+const simpleCalendarModule = "foundryvtt-simple-calendar";
+
 export async function setupSimpleCalendar() {
-  const simpleCalendarModule = "foundryvtt-simple-calendar";
-
   if (game.modules.get(simpleCalendarModule)?.active) {
-    // that file is an export of simple-calendar settings
-    const calendar = await import("./simple-calendar-glorantha.json");
-    const calendarSettings = (calendar as any).default;
-
-    console.log("RQG | Configuring simple calendar for Glorantha");
-
-    if (calendarSettings.hasOwnProperty("yearSettings")) {
-      await game.settings.set(simpleCalendarModule, "year-config", calendarSettings.yearSettings);
-    }
-    if (calendarSettings.hasOwnProperty("monthSettings")) {
-      await game.settings.set(simpleCalendarModule, "month-config", calendarSettings.monthSettings);
-    }
-    if (calendarSettings.hasOwnProperty("weekdaySettings")) {
-      await game.settings.set(
-        simpleCalendarModule,
-        "weekday-config",
-        calendarSettings.weekdaySettings
-      );
-    }
-    if (calendarSettings.hasOwnProperty("leapYearSettings")) {
-      await game.settings.set(
-        simpleCalendarModule,
-        "leap-year-rule",
-        calendarSettings.leapYearSettings
-      );
-    }
-    if (calendarSettings.hasOwnProperty("timeSettings")) {
-      await game.settings.set(
-        simpleCalendarModule,
-        "time-configuration",
-        calendarSettings.timeSettings
-      );
-    }
-    if (calendarSettings.hasOwnProperty("seasonSettings")) {
-      await game.settings.set(
-        simpleCalendarModule,
-        "season-configuration",
-        calendarSettings.seasonSettings
-      );
-    }
-    if (calendarSettings.hasOwnProperty("moonSettings")) {
-      await game.settings.set(
-        simpleCalendarModule,
-        "moon-configuration",
-        calendarSettings.moonSettings
-      );
-    }
-    if (calendarSettings.hasOwnProperty("generalSettings")) {
-      await game.settings.set(
-        simpleCalendarModule,
-        "general-configuration",
-        calendarSettings.generalSettings
-      );
-    }
-    // if (calendarSettings.hasOwnProperty("currentDate")) {
-    //   await game.settings.set(simpleCalendarModule, "current-date", calendarSettings.currentDate);
-    // }
+    const calendar = await import("./simple-calendar-glorantha.json"); // that file is an export of simple-calendar settings with current date removed
+    SimpleCalendar.api.configureCalendar(calendar);
+    return;
   }
 }
