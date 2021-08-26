b57de9412c0940f3abd67ad3d4613fd2d14e4c0f
Åke Wivänge <ake.wivange@pagero.com>
2021-08-26 23:48:07 +0200

    refactor: import simple-calendar settings as a variable instead of as a json file

--- src/module-integration/simple-calendar-init.ts
+++ src/module-integration/simple-calendar-init.ts
@@ -1,10 +1,11 @@
+import { calendar } from "./simple-calendar-glorantha";
+
 declare const SimpleCalendar: any;
 const simpleCalendarModule = "foundryvtt-simple-calendar";
 
 export async function setupSimpleCalendar() {
   if (game.modules.get(simpleCalendarModule)?.active) {
-    const calendar = await import("./simple-calendar-glorantha.json"); // that file is an export of simple-calendar settings with current date removed
-    SimpleCalendar.api.configureCalendar(calendar.default);
+    SimpleCalendar.api.configureCalendar(calendar); // calendar is basically an export of simple-calendar settings with current date removed
     return;
   }
 }
