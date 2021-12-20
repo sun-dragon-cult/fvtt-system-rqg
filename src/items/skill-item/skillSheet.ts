b0dcdaec30565dbd931f625307d01fdbdae09bf1
Moonpile <moonpile@users.noreply.github.com>
2021-12-20 10:28:45 -0500

    - removed a commented out line - applied Prettier formatting manually

--- src/items/skill-item/skillSheet.ts
+++ src/items/skill-item/skillSheet.ts
@@ -112,7 +112,11 @@ export class SkillSheet extends RqgItemSheet<ItemSheet.Options, SkillSheetData |
     }
   }
 
-  static async showImproveSkillDialog(actor: RqgActor, skillItemId: string, speakerName: string): Promise<void> {
+  static async showImproveSkillDialog(
+    actor: RqgActor,
+    skillItemId: string,
+    speakerName: string
+  ): Promise<void> {
     const item = actor.items.get(skillItemId);
     if (!item || item.data.type !== ItemTypeEnum.Skill) {
       const msg = `Couldn't find skill with itemId [${skillItemId}] on actor ${actor.name} to show Improve Skill dialog.`;
@@ -120,7 +124,7 @@ export class SkillSheet extends RqgItemSheet<ItemSheet.Options, SkillSheetData |
       throw new RqgError(msg);
     }
     const skill = item.data as SkillDataProperties;
-    
+
     if (item.data.data.chance > 75) {
       //@ts-ignore hideTraining
       item.data.data.hideTraining = true;
@@ -132,39 +136,44 @@ export class SkillSheet extends RqgItemSheet<ItemSheet.Options, SkillSheetData |
     if (chance > 100) {
       chanceToGain = categoryMod;
     } else {
-      chanceToGain = (100 - chance) + categoryMod;
+      chanceToGain = 100 - chance + categoryMod;
     }
 
     const btnImprove = getGame().i18n.format("DIALOG.improveSkillDialog.btnDoImprovement");
     const btnCancel = getGame().i18n.format("DIALOG.improveSkillDialog.btnCancel");
     const buttons: any = {};
-    
+
     if (skill.data.hasExperience || skill.data.chance < 75) {
       buttons.submit = {
         icon: '<i class="fas fa-check"></i>',
         label: btnImprove,
         callback: async (html: JQuery | HTMLElement) =>
-          await SkillSheet.submitImproveSkillDialog(
-            html as JQuery,
-            actor,
-            item,
-            speakerName
-          )
-      }
+          await SkillSheet.submitImproveSkillDialog(html as JQuery, actor, item, speakerName),
+      };
     }
-    
+
     buttons.cancel = {
       icon: '<i class="fas fa-times"></i>',
       label: btnCancel,
       callback: () => null,
-    }
+    };
 
     const experience1d6Checked = skill.data.hasExperience;
-    const training1d6minus1Checked =  ((!experience1d6Checked) ? true : false);
+    const training1d6minus1Checked = !experience1d6Checked ? true : false;
 
-    const content: string = await renderTemplate("systems/rqg/items/skill-item/dialogShowImproveSkill.hbs", {skill: skill, chanceToGain: chanceToGain, experience1d6Checked: experience1d6Checked, training1d6minus1Checked: training1d6minus1Checked})
-    
-    const title = getGame().i18n.format("DIALOG.improveSkillDialog.title", {skillName: skill.data.skillName});
+    const content: string = await renderTemplate(
+      "systems/rqg/items/skill-item/dialogShowImproveSkill.hbs",
+      {
+        skill: skill,
+        chanceToGain: chanceToGain,
+        experience1d6Checked: experience1d6Checked,
+        training1d6minus1Checked: training1d6minus1Checked,
+      }
+    );
+
+    const title = getGame().i18n.format("DIALOG.improveSkillDialog.title", {
+      skillName: skill.data.skillName,
+    });
     new Dialog(
       {
         title: title,
@@ -175,7 +184,7 @@ export class SkillSheet extends RqgItemSheet<ItemSheet.Options, SkillSheetData |
       {
         classes: ["rqg", "dialog"],
       }
-    ).render(true);      
+    ).render(true);
   }
 
   private static async submitImproveSkillDialog(
@@ -196,87 +205,118 @@ export class SkillSheet extends RqgItemSheet<ItemSheet.Options, SkillSheetData |
       if (skillData.data.hasExperience) {
         var chance: number = Number(item.data.data.chance) || 0;
         var categoryMod: number = Number(item.data.data.categoryMod) || 0;
-        const rollFlavor = getGame().i18n.format("DIALOG.improveSkillDialog.experienceRoll.flavor", {actorName: actor.name, skillName: skillData.data.skillName});
-        const rollContent = getGame().i18n.format("DIALOG.improveSkillDialog.experienceRoll.content", {mod: categoryMod, skillChance: skillData.data.chance});
-        // const result = await Ability.roll(flavor, chance, categoryMod, speakerName);
+        const rollFlavor = getGame().i18n.format(
+          "DIALOG.improveSkillDialog.experienceRoll.flavor",
+          { actorName: actor.name, skillName: skillData.data.skillName }
+        );
+        const rollContent = getGame().i18n.format(
+          "DIALOG.improveSkillDialog.experienceRoll.content",
+          { mod: categoryMod, skillChance: skillData.data.chance }
+        );
 
         // roll 1d100 and add the category mod
         const expRoll = new Roll("1d100+" + categoryMod);
         await expRoll.toMessage({
-          speaker: {alias: speakerName},
+          speaker: { alias: speakerName },
           type: CONST.CHAT_MESSAGE_TYPES.ROLL,
-          flavor: `<h3>${rollFlavor}</h3><p>${rollContent}</p>`
+          flavor: `<h3>${rollFlavor}</h3><p>${rollContent}</p>`,
         });
 
         // Gain if the modified d100 roll is greater than (but not equal to) the skill chance, or if the roll is greater than or equal to 100
-        if ( expRoll.total !== undefined && (expRoll.total > skillData.data.chance || expRoll.total >= 100)) {
+        if (
+          expRoll.total !== undefined &&
+          (expRoll.total > skillData.data.chance || expRoll.total >= 100)
+        ) {
           // increase skill learnedChance, clear experience check
-          const resultFlavor = getGame().i18n.format("DIALOG.improveSkillDialog.experienceResultCard.flavor", {skillName: skillData.data.skillName});
+          const resultFlavor = getGame().i18n.format(
+            "DIALOG.improveSkillDialog.experienceResultCard.flavor",
+            { skillName: skillData.data.skillName }
+          );
           if (gaintype === "experience3") {
-            const resultContentChose3 = getGame().i18n.format("DIALOG.improveSkillDialog.experienceResultCard.contentChose3");
+            const resultContentChose3 = getGame().i18n.format(
+              "DIALOG.improveSkillDialog.experienceResultCard.contentChose3"
+            );
             const gainRoll = new Roll("3");
             await gainRoll.toMessage({
-              speaker: { alias: speakerName},
+              speaker: { alias: speakerName },
               type: CONST.CHAT_MESSAGE_TYPES.ROLL,
-              flavor: `<h3>${resultFlavor}</h3><p>${resultContentChose3}</p>`
+              flavor: `<h3>${resultFlavor}</h3><p>${resultContentChose3}</p>`,
             });
             gain = 3;
           }
           if (gaintype === "experience1d6") {
-            const resultContentChose1d6 = getGame().i18n.format("DIALOG.improveSkillDialog.experienceResultCard.contentChose1d6");
+            const resultContentChose1d6 = getGame().i18n.format(
+              "DIALOG.improveSkillDialog.experienceResultCard.contentChose1d6"
+            );
             const gainRoll = new Roll("1d6");
             await gainRoll.toMessage({
-              speaker: { alias: speakerName},
+              speaker: { alias: speakerName },
               type: CONST.CHAT_MESSAGE_TYPES.ROLL,
-              flavor: `<h3>${resultFlavor}</h3><p>${resultContentChose1d6}</p>`
+              flavor: `<h3>${resultFlavor}</h3><p>${resultContentChose1d6}</p>`,
             });
-            gain = Number(gainRoll.total) || 0; 
+            gain = Number(gainRoll.total) || 0;
           }
           ui.notifications?.info(`${actor.name} gained ${gain}% in ${item.name}!`);
         } else {
-          // no increase, clear experience check 
+          // no increase, clear experience check
           gain = 0;
-          const failedFlavor = getGame().i18n.format("DIALOG.improveSkillDialog.experienceGainFailed.flavor", {skillName: skillData.data.skillName});
-          const failedContent = getGame().i18n.format("DIALOG.improveSkillDialog.experienceGainFailed.content", {actorName: actor.name, skillName: skillData.data.skillName})
+          const failedFlavor = getGame().i18n.format(
+            "DIALOG.improveSkillDialog.experienceGainFailed.flavor",
+            { skillName: skillData.data.skillName }
+          );
+          const failedContent = getGame().i18n.format(
+            "DIALOG.improveSkillDialog.experienceGainFailed.content",
+            { actorName: actor.name, skillName: skillData.data.skillName }
+          );
           const failChat = {
             type: CONST.CHAT_MESSAGE_TYPES.OTHER,
             flavor: failedFlavor,
             content: failedContent,
-            speaker: {alias: speakerName}
+            speaker: { alias: speakerName },
           };
           ChatMessage.create(failChat);
         }
       } else {
-        const msg = getGame().i18n.format("DIALOG.improveSkillDialog.notifications.noExperience", {actorName: actor.name, skillName: skillData.data.skillName});
+        const msg = getGame().i18n.format("DIALOG.improveSkillDialog.notifications.noExperience", {
+          actorName: actor.name,
+          skillName: skillData.data.skillName,
+        });
         ui.notifications?.error(msg);
       }
     }
     if (gaintype === "training2") {
-      const flavor = getGame().i18n.format("DIALOG.improveSkillDialog.trainingResultCard.flavor", {skillName: skillData.data.skillName});
-      const content = getGame().i18n.format("DIALOG.improveSkillDialog.trainingResultCard.contentChose2");
+      const flavor = getGame().i18n.format("DIALOG.improveSkillDialog.trainingResultCard.flavor", {
+        skillName: skillData.data.skillName,
+      });
+      const content = getGame().i18n.format(
+        "DIALOG.improveSkillDialog.trainingResultCard.contentChose2"
+      );
       const roll = new Roll("2");
       await roll.toMessage({
-        speaker: { alias: speakerName},
+        speaker: { alias: speakerName },
         type: CONST.CHAT_MESSAGE_TYPES.ROLL,
-        flavor: `<h3>${flavor}</h3><p>${content}</p>`
+        flavor: `<h3>${flavor}</h3><p>${content}</p>`,
       });
       gain = 2;
     }
     if (gaintype === "training1d6minus1") {
-      const flavor = getGame().i18n.format("DIALOG.improveSkillDialog.trainingResultCard.flavor", {skillName: skillData.data.skillName});
-      const content = getGame().i18n.format("DIALOG.improveSkillDialog.trainingResultCard.contentChose1d6minus1");
+      const flavor = getGame().i18n.format("DIALOG.improveSkillDialog.trainingResultCard.flavor", {
+        skillName: skillData.data.skillName,
+      });
+      const content = getGame().i18n.format(
+        "DIALOG.improveSkillDialog.trainingResultCard.contentChose1d6minus1"
+      );
       const gainRoll = new Roll("1d6-1");
       await gainRoll.toMessage({
-        speaker: { alias: speakerName},
+        speaker: { alias: speakerName },
         type: CONST.CHAT_MESSAGE_TYPES.ROLL,
-        flavor: `<h3>${flavor}</h3><p>${content}</p>`
+        flavor: `<h3>${flavor}</h3><p>${content}</p>`,
       });
-      gain = Number(gainRoll.total) || 0; 
+      gain = Number(gainRoll.total) || 0;
     }
     let newLearnedChance: number = Number(skillData.data.learnedChance) + gain;
     await actor.updateEmbeddedDocuments("Item", [
-      { _id: item.id, data: { hasExperience: false, learnedChance: newLearnedChance } }
+      { _id: item.id, data: { hasExperience: false, learnedChance: newLearnedChance } },
     ]);
-
   }
 }
