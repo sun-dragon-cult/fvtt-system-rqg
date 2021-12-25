3d7885522c7f1337e4b57b8693dea1fb2c1f5432
Moonpile <3959760+Moonpile@users.noreply.github.com>
2021-12-25 14:58:17 -0500

    feat Implement improvement for Passions, Runes, and Characteristics (#113)
    * - Moved improveSkillDialog to src/dialog and renamed it improveAbilityDialog
    - Context menu "Improve" can display dialog for Passions, Runes, and Skills but doesn't yet display for characteristics
    - Submit button callback is still just a stub.
    
    * - getting showImproveCharacteristicDialog
    - submit callback is still a stub
    - about to merge from origin/master
    
    * - Species Max for characteristics
    - Research for STR, CON, DEX, CHA
    - submit callback still a stub
    
    * - Improve always shows on Rune Context Menu
    - submitImproveAbilityDialog now improves Passions, Runes, and Skills
    - submitImproveCharacteristicDialog for Characteristics is still a stub
    
    * feat Implement improvement of Runes, Passions, and POW (issues 98, 99, 100) plus "research improvements" for STR, CON, DEX, and CHA
    
    - Important note: this does not currently factor ENC penalties out of skill totals, so it's best to do your training in the nude!
    - Also the "result cards" could be jazzed up but that will wait for the overall effort to deal with that.
    
    * Implement Improvement of POW
    Fixes #98
    
    - used item.data.type and ItemTypeEnum to make comparisons
    - Moved improve Characteristic to it's own file
    - export the functions directly rather than as part of a class
    
    Co-authored-by: Moonpile <moonpile@users.noreply.github.com>

--- src/actors/context-menues/characteristic-context-menu.ts
+++ src/actors/context-menues/characteristic-context-menu.ts
@@ -3,6 +3,7 @@ import { RqgActor } from "../rqgActor";
 import { Characteristic, Characteristics } from "../../data-model/actor-data/characteristics";
 import { getDomDataset, getGame, getGameUser, requireValue, RqgError } from "../../system/util";
 import { ActorDataConstructorData } from "@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/data.mjs/actorData";
+import { showImproveCharacteristicDialog } from "../../dialog/improveCharacteristicDialog";
 
 export const characteristicMenuOptions = (
   actor: RqgActor,
@@ -59,10 +60,21 @@ export const characteristicMenuOptions = (
     icon: '<i class="fas fa-arrow-alt-circle-up"></i>',
     condition: (el: JQuery): boolean => {
       const { name: characteristicName, value: characteristic } = getCharacteristic(actor, el);
-      return !!(characteristicName === "power" && characteristic?.hasExperience);
+      // You can train STR, CON, DEX, POW, and CHA, and you can increase POW via experience
+      // You cannot train INT or increase it via experience
+      
+      const trainable = ["strength", "constitution", "dexterity", "power", "charisma"];
+      return !!(trainable.includes(characteristicName));
     },
     callback: (el: JQuery) => {
-      ui.notifications?.info("TODO Improve");
+      console.log(el);
+      const charName = getDomDataset(el, "characteristic");
+      requireValue(charName, "Couldn't find dataset for characteristic");
+
+      const characteristic = (actor.data.data.characteristics as any)[charName];
+      characteristic.name = charName;
+      const speakerName = token?.name ?? actor.data.token.name ?? "";
+      showImproveCharacteristicDialog(actor, "characteristic", characteristic, speakerName);
     },
   },
   {
