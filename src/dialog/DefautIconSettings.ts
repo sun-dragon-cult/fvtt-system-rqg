18786ff32fca9af7cdc9ce2084bd51f659697ebc
Moonpile <moonpile@users.noreply.github.com>
2021-12-26 12:21:44 -0500

    - More steps toward the default icon settings FormApplication working (but it doesn't yet)

--- src/dialog/DefautIconSettings.ts
+++ src/dialog/DefautIconSettings.ts
@@ -1,17 +1,38 @@
 import { getGame } from "../system/util";
 
 export class DefaultIconSettings extends FormApplication {
-
-    getData(options?: Application.RenderOptions): FormApplication.Data<{}, FormApplication.Options> | Promise<FormApplication.Data<{}, FormApplication.Options>> {
-        const data: FormApplication.Data<{}, FormApplication.Options>  =  getGame().settings.get('rqg', "defaultIconSettings") as FormApplication.Data<{}, FormApplication.Options> ;
-        return data;
-    }
-  
-    protected _updateObject(event: Event, formData?: object): Promise<unknown> {
-      if (formData !== undefined) {
-        const data = expandObject(formData);
-        getGame().settings.set("rqg", "defaultIconSettings", data);
-      }
-      return Promise.resolve();
-    }
+  constructor(options: any) {
+    super(options);
+    this.options = options;
   }
+
+  static get defaultOptions(): FormApplication.Options {
+      return mergeObject(super.defaultOptions, {
+        classes: ['form'],
+        popOut: true,
+        template: 'defaultIconSettings.hbs',
+        id: "default-icons-settings-dialog",
+        title: "Default Icon Settings",
+      })
+  }
+
+  getData(
+    options?: Application.RenderOptions
+  ):
+    | FormApplication.Data<{}, FormApplication.Options>
+    | Promise<FormApplication.Data<{}, FormApplication.Options>> {
+    const data: FormApplication.Data<{}, FormApplication.Options> = getGame().settings.get(
+      "rqg",
+      "defaultIconSettings"
+    ) as FormApplication.Data<{}, FormApplication.Options>;
+    return data;
+  }
+
+  protected _updateObject(event: Event, formData?: object): Promise<unknown> {
+    if (formData !== undefined) {
+      const data = expandObject(formData);
+      getGame().settings.set("rqg", "defaultIconSettings", data);
+    }
+    return Promise.resolve();
+  }
+}
