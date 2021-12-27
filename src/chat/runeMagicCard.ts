b073ab6988be2e8c6bad5658f9a24fe7e15f7ea7
Moonpile <moonpile@users.noreply.github.com>
2021-12-27 15:10:00 -0500

    - added data-rune-magic-roll class to trigger rune magic rolls from actor sheet - added runeMagicCard.ts and runeMagicCard.hbs - The card gets the eligible runes (either from the spell or from the cult for common rune magic) and lists them to choose from - still need to actually do the roll, deduct the Rune Points, and add in any modifiers. - still needs localization

---
+++ src/chat/runeMagicCard.ts
@@ -0,0 +1,178 @@
+import { ItemDataSource } from "@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/data.mjs/itemData";
+import { Rune } from "../actors/item-specific/rune";
+import { RqgActor } from "../actors/rqgActor";
+import { RqgActorDataSource } from "../data-model/actor-data/rqgActorData";
+import { CultDataSource } from "../data-model/item-data/cultData";
+import { ItemTypeEnum, RqgItemDataSource } from "../data-model/item-data/itemTypes";
+import { RuneTypeEnum } from "../data-model/item-data/runeData";
+import { Ability } from "../data-model/shared/ability";
+import {
+  assertItemType,
+  getActorFromIds,
+  getGame,
+  getSpeakerName,
+  RqgError,
+  usersThatOwnActor,
+} from "../system/util";
+
+type RuneMagicCardFlags = {
+  actorId: string;
+  tokenId: string | null; // Needed to avoid saving a full (possibly syntetic) actor
+  itemData: ItemDataSource;
+  formData: {
+    runePointCost: number;
+    runes: RuneForCasting[];
+  };
+};
+
+type RuneForCasting = {
+    rune: string,
+    actorRune: Rune,
+}
+
+export class RuneMagicCard {
+  public static async show(
+    runeMagicItemId: string,
+    actor: RqgActor,
+    token: TokenDocument | null
+  ): Promise<void> {
+    const runeMagicItem = actor.items.get(runeMagicItemId);
+    assertItemType(runeMagicItem?.data.type, ItemTypeEnum.RuneMagic);
+    const cult = actor.items.get(runeMagicItem.data.data.cultId);
+    assertItemType(cult?.data.type, ItemTypeEnum.Cult);
+    if (!actor.id) {
+      const msg = `Actor without id in rune magic card`;
+      ui.notifications?.error(msg);
+      throw new RqgError(msg, actor);
+    }
+    let usableRunes = [];
+    let runesForCasting: RuneForCasting[] = [];
+    if (runeMagicItem.data.data.runes.includes("Magic (condition)")) {
+        // Actor can use any of the cult's runes to cast
+        // And some cults have the same rune more than once, so de-dupe them
+        usableRunes =  [...new Set(cult.data.data.runes)];
+    } else {
+        // Actor can use any of the Rune Magic Spell's runes to cast
+        usableRunes = [...new Set(runeMagicItem.data.data.runes)];
+    }
+    usableRunes.forEach(cultRune => {
+        const actorRune = actor.items.getName(cultRune);
+        assertItemType(actorRune?.data.type, ItemTypeEnum.Rune);
+        const runeForCasting: RuneForCasting = {
+            rune: cultRune,
+            actorRune: actorRune,
+        }
+        runesForCasting.push(runeForCasting);
+    });
+
+    const flags: RuneMagicCardFlags = {
+      actorId: actor.id,
+      tokenId: token?.id ?? null,
+      itemData: runeMagicItem.data.toObject(),
+      formData: {
+        runePointCost: runeMagicItem.data.data.points,
+        runes: runesForCasting,
+      },
+    };
+
+    ui?.sidebar?.tabs.chat && ui.sidebar?.activateTab(ui?.sidebar.tabs.chat.tabName);
+    await ChatMessage.create(await this.renderContent(flags));
+  }
+
+  public static async inputChangeHandler(ev: Event, messageId: string): Promise<void> {}
+
+  public static async formSubmitHandler(
+    ev: JQueryEventObject,
+    messageId: string
+  ): Promise<boolean> {
+    ev.preventDefault();
+
+    const chatMessage = getGame().messages?.get(messageId);
+    const flags = chatMessage?.data.flags.rqg as RuneMagicCardFlags;
+
+    const formData = new FormData(ev.target as HTMLFormElement);
+    // @ts-ignore formData.entries()
+    for (const [name, value] of formData.entries()) {
+      if (name in flags.formData) {
+        flags.formData[name as keyof typeof flags.formData] = value;
+      }
+    }
+
+    // @ts-ignore submitter
+    const button = ev.originalEvent.submitter as HTMLButtonElement;
+    button.disabled = true;
+    setTimeout(() => (button.disabled = false), 1000); // Prevent double clicks
+
+    const actor = getActorFromIds(flags.actorId, flags.tokenId);
+    if (actor) {
+      const speakerName = getSpeakerName(flags.actorId, flags.tokenId);
+      await RuneMagicCard.roll(flags.itemData, 0, actor, speakerName);
+    } else {
+      ui.notifications?.warn("Couldn't find world actor to do rune magic roll");
+    }
+    return false;
+  }
+
+
+  public static async roll(
+      itemData: ItemDataSource,
+      runePointCost: number,
+      actor: RqgActor,
+      speakerName: string, 
+  ): Promise<void> {
+    assertItemType(itemData.type, ItemTypeEnum.RuneMagic);
+    const cult = actor.items.get(itemData.data.cultId);
+    assertItemType(cult?.data.type, ItemTypeEnum.Cult);
+    const actorData = actor.data;
+    const validationError = RuneMagicCard.validateData(
+        actorData,
+        itemData,
+        cult.data,
+        runePointCost,
+    )
+    if (validationError) {
+        ui.notifications?.warn(validationError);
+    } else {
+        const result = await Ability.roll(
+            "Cast " + itemData.name,
+            50,
+            0,
+            speakerName
+        )
+    }
+  }
+
+  
+  public static validateData(
+    actorData: RqgActorDataSource,
+    itemData: RqgItemDataSource,
+    cultData: CultDataSource,
+    runePointCost: number,
+  ): string {
+    assertItemType(itemData.type, ItemTypeEnum.SpiritMagic);
+    if (runePointCost > itemData.data.points) {
+      return "Can not cast spell above learned level"; //TODO: Does this apply to Rune Magic?
+    } else if (runePointCost > (cultData.data.runePoints.value || 0)) {
+      return "Not enough rune points left";
+    } else {
+      return "";
+    }
+  }
+
+  private static async renderContent(flags: RuneMagicCardFlags): Promise<object> {
+    let html = await renderTemplate("systems/rqg/chat/runeMagicCard.hbs", flags);
+    const speakerName = getSpeakerName(flags.actorId, flags.tokenId);
+    return {
+      flavor: "Rune Magic: " + flags.itemData.name,
+      user: getGame().user?.id,
+      speaker: { alias: speakerName },
+      content: html,
+      whisper: usersThatOwnActor(getActorFromIds(flags.actorId, flags.tokenId)),
+      type: CONST.CHAT_MESSAGE_TYPES.WHISPER,
+      flags: {
+        core: { canPopout: true },
+        rqg: flags,
+      },
+    };
+  }
+}
