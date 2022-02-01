db9d8a58646c53396444897d40ef1c21469be735
Åke Wivänge <ake.wivange@pagero.com>
2022-02-01 08:37:51 +0100

    feat: Expand RuneMagicItem datamodel #148

--- src/data-model/item-data/runeMagicData.ts
+++ src/data-model/item-data/runeMagicData.ts
@@ -1,32 +1,15 @@
 import { JournalEntryLink } from "../shared/journalentrylink";
 import { ItemTypeEnum } from "./itemTypes";
+import { Spell, SpellConcentrationEnum, SpellDurationEnum, SpellRangeEnum } from "./spell";
 
-export enum RuneMagicCastingRangeEnum {
-  Self = "self",
-  Touch = "touch",
-  Ranged = "ranged", // default 160 meters
-}
-
-export enum RuneMagicDurationEnum {
-  Instant = "instant",
-  Temporal = "temporal", // default 15 minutes
-  Duration = "duration", // Length of pregnancy / 12 hours / variable etc
-  Focused = "focused", // Active for as long as the caster focuses
-  Permanent = "permanent", // Ritual (Enchantment)
-}
-
-export interface RuneMagicDataSourceData extends JournalEntryLink {
+export interface RuneMagicDataSourceData extends JournalEntryLink, Spell {
   /** The cult this rune magic is learned from and where to draw rune points */
   cultId: string;
   /** Array of rune names like "Man (form)" */
   runes: string[];
-  /** Learned strength */
-  points: number;
-  castingRange: RuneMagicCastingRangeEnum;
-  duration: RuneMagicDurationEnum;
-  isRitual: boolean;
   /** Can the caster decide the number of rune points used */
   isStackable: boolean;
+  /** Reduce max POW by the number of RP cast */
   isOneUse: boolean;
 }
 
@@ -50,11 +33,13 @@ export const emptyRuneMagic: RuneMagicDataSourceData = {
   cultId: "",
   runes: [],
   points: 0,
-  castingRange: RuneMagicCastingRangeEnum.Ranged,
-  duration: RuneMagicDurationEnum.Temporal,
+  castingRange: SpellRangeEnum.Ranged,
+  duration: SpellDurationEnum.Temporal,
+  concentration: SpellConcentrationEnum.Passive,
   isRitual: false,
   isStackable: false,
   isOneUse: false,
+  isEnchantment: false,
   journalId: "",
   journalPack: "",
 };
