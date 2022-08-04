d3d9e87914612ed0d6affbb290e55bc6c269fa23
Åke Wivänge <ake.wivange@pagero.com>
2022-08-04 14:29:45 +0200

    fix: change name of Fire rune to Fire/Sky (#306)

---
+++ src/system/migrations/migrations-item/renameFireSky.ts
@@ -0,0 +1,40 @@
+import { ItemData } from "@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/data.mjs";
+import { ItemUpdate } from "../applyMigrations";
+
+export async function renameFireSky(itemData: ItemData): Promise<ItemUpdate> {
+  let updateData: ItemUpdate = {};
+
+  // Rename the Fire rune item itself
+  if (itemData.name === "Fire (element)") {
+    updateData = {
+      name: "Fire/Sky (element)",
+      data: { rune: "Fire/Sky" },
+      flags: { rqg: { documentRqidFlags: { id: "i.rune.fire-sky-element" } } },
+    };
+  }
+
+  // Rename Rune item references to the Fire rune
+  // A Rune can not have itself as a minor rune so there is no risk of overwriting updateData above
+  const minorRunes = (itemData.data as any).minorRunes;
+  if (minorRunes?.includes("Fire (element)")) {
+    updateData = {
+      data: {
+        minorRunes: minorRunes.map((r: string) =>
+          r === "Fire (element)" ? "Fire/Sky (element)" : r
+        ),
+      },
+    };
+  }
+
+  // Cult, RuneMagic & Skill items have "runes" property that might point to Fire Rune
+  const runes = (itemData.data as any).runes;
+  if (runes?.includes("Fire (element)")) {
+    updateData = {
+      data: {
+        runes: runes.map((r: string) => (r === "Fire (element)" ? "Fire/Sky (element)" : r)),
+      },
+    };
+  }
+
+  return updateData;
+}
