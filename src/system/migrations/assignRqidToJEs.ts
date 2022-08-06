1c7da7b92f914ea9bceb58324f8c42cc33bc93c4
Åke Wivänge <ake.wivange@pagero.com>
2022-08-06 14:51:35 +0200

    chore: automate running the script to assign rqid to journal entries as part of the migration process

---
+++ src/system/migrations/assignRqidToJEs.ts
@@ -0,0 +1,75 @@
+import { Document } from "@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/abstract/module.mjs";
+import { getGame } from "../util";
+import { Rqid } from "../api/rqidApi";
+
+export async function assignRqidToJEs(): Promise<void> {
+  console.info("--- Assigning rqid to world journal entries ---");
+  for (const je of getGame()?.journal ?? []) {
+    const rqidFlags = je.getFlag("rqg", "documentRqidFlags") ?? {};
+
+    console.debug("Existing rqid Flags in world", rqidFlags);
+    // Add a rqid if it does not already have one
+    if (!rqidFlags?.id) {
+      rqidFlags.id = await generateRqid(je, "world");
+      rqidFlags.lang = rqidFlags?.lang ? rqidFlags.lang : "en";
+      rqidFlags.priority = rqidFlags?.priority ? rqidFlags.priority : 19;
+
+      console.log("...setting world rqid", rqidFlags);
+      await je.setFlag("rqg", "documentRqidFlags", rqidFlags);
+    }
+  }
+
+  console.info("--- Assigning rqid to compendia journal entries ---");
+  for (const pack of getGame()?.packs) {
+    if (pack.documentClass.name === "JournalEntry") {
+      // @ts-expect-error indexed
+      if (!pack.indexed) {
+        await pack.getIndex();
+      }
+      for (const jei of pack.index) {
+        // @ts-expect-error flags
+        const rqidFlags = jei?.flags?.rqg?.documentRqidFlags ?? {};
+
+        console.debug(
+          "Existing rqid Flags in compendium",
+          pack.metadata.package,
+          pack.metadata.name,
+          rqidFlags
+        );
+        // Add a rqid if it does not already have one
+        if (!rqidFlags?.id) {
+          // @ts-expect-error _id
+          const je = (await pack.getDocument(jei._id)) as Document<any, any> | undefined;
+          if (!je) {
+            console.error("Couldn't get journal entry from pack!!!");
+            continue;
+          }
+          rqidFlags.id = await generateRqid(je, "compendiums");
+          rqidFlags.lang = rqidFlags?.lang ? rqidFlags.lang : "en";
+          rqidFlags.priority = rqidFlags?.priority ? rqidFlags.priority : 1999;
+
+          console.log("...setting compendium rqid", rqidFlags);
+
+          const wasLocked = pack.locked;
+          await pack.configure({ locked: false });
+          await je.setFlag("rqg", "documentRqidFlags", rqidFlags);
+          // Apply the original locked status for the pack
+          await pack.configure({ locked: wasLocked });
+        }
+      }
+    }
+  }
+  console.info("--- Done assigning rqids to journal entries ---");
+
+  /**
+   * Generate a unique rqid for the JournalEntry.
+   * Assumes the JE does not yet have a rqid.
+   * Also assumes english only rqids.
+   **/
+  async function generateRqid(je: Document<any, any>, scope: "all" | "world" | "compendiums") {
+    const proposedRqid = Rqid.getDefaultRqid(je);
+    const count = await Rqid.fromRqidCount(proposedRqid, "en", scope);
+    const postfixCount = !!count ? "-" + (Number(count) + 1) : "";
+    return `${proposedRqid}${postfixCount}`;
+  }
+}
--- src/system/migrations/migrateWorld.ts
+++ src/system/migrations/migrateWorld.ts
@@ -9,6 +9,7 @@ import { systemId } from "../config";
 import { renameDragonewt } from "./migrations-item/renameDragonewt";
 import { useRqidDescriptionLinks } from "./migrations-item/migrateDescriptionLinks";
 import { renameFireSky } from "./migrations-item/renameFireSky";
+import { assignRqidToJEs } from "./assignRqidToJEs";
 
 /**
  * Perform a system migration for the entire World, applying migrations for what is in it
@@ -17,6 +18,7 @@ export async function migrateWorld(): Promise<void> {
   if (
     getGame().system.data.version !== getGame().settings.get(systemId, "systemMigrationVersion")
   ) {
+    await confirmRunAssignRqidDialog(getGame().system.data.version);
     ui.notifications?.info(
       `Applying RQG System Migration for version ${
         getGame().system.data.version
@@ -52,4 +54,56 @@ export async function applyDefaultWorldMigrations(): Promise<void> {
   const worldActorMigrations: ActorMigration[] = [migrateActorDummy];
 
   await applyMigrations(worldItemMigrations, worldActorMigrations);
+}
+
+// This is only for this update - since you need to make sure you have tagged the journal entries with rqid before migration.
+async function confirmRunAssignRqidDialog(newVersion: string): Promise<void> {
+  return await new Promise(async (resolve) => {
+    const title = `Migrating RQG system to version ${newVersion}`;
+    const content = `
+      <h1>Please read before continuing!</h1>
+      <p>This version introduces a new internal id (Rqid) that is used for links between documents like items, journal entries etc. This migration will update the
+      current (old style) description links from items to journal entry descriptions to use the new Rqids.</p>
+
+      <h2><b>Step 1</b> – Tagging your journal entries with Rqid</h2>
+      <p>To make your current description links work, your existing journal entries needs to be tagged with a "Rqid" so they can be linked to. The <b>Assign Rqid IDs</b> button
+       will go through all your world and compendium journal entries and assign a rqid to each of them.
+       <b>Please do this before continuing with the migration!</b></p>
+       <p>When this is finished the browser will reload, and you will end up on this screen again. It is not destructive to run it multiple times (but once is enough 😊).</p>
+
+      <h2><b>Step 2</b> – Running the main migration</h2>
+      <p>After the tagging is done you are set to continue with the migration by clicking <b>Run Migration</b>. This main migration will try to follow your current description
+      links and convert them to using a Rqid link instead (using the Rqid that was assigned to your journal entries in the first step). It will also apply other more minor
+      migrations needed to run this new version.</p>
+      <p>Please note that this can take some time if you have a world with many items, actors etc.</p>
+      <p>Opening the browser console log (F12) will show you warnings about description migrations that could not take place. This should only happen if the old link wasn't working.
+      For a large world this will most likely be too much to go through, but can be examined if you want to find descriptions that don't work.</p>`;
+    const dialog = new Dialog(
+      {
+        title: title,
+        content: content,
+        default: "submit",
+        buttons: {
+          submit: {
+            icon: '<i class="fa fa-tags"></i>',
+            label: "<b>Step 1</b> – Assign Rqid IDs",
+            callback: async () => {
+              await assignRqidToJEs();
+              console.log("*** Reloading browser to force pack index update ***");
+              location.reload();
+            },
+          },
+          cancel: {
+            label: "<b>Step 2</b> – Run Migration",
+            icon: '<i class="fa fa-arrow-right"></i>',
+            callback: () => {
+              resolve();
+            },
+          },
+        },
+      },
+      { width: 600 }
+    );
+    await dialog.render(true);
+  });
 }
