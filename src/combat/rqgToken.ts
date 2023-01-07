4bb74e28800d442e7bcaff3986238c002251ca25
Moonpile <3959760+Moonpile@users.noreply.github.com>
2023-01-07 12:07:00 -0500

    feat: initialize Actor for new Token if Actor is unlinked Fixes #415

--- src/combat/rqgToken.ts
+++ src/combat/rqgToken.ts
@@ -1,3 +1,9 @@
+//@ts-nocheck
+import { DocumentModificationOptions } from "@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/abstract/document.mjs";
+import { TokenDataProperties } from "@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/data.mjs/tokenData";
+import { PropertiesToSource } from "@league-of-foundry-developers/foundry-vtt-types/src/types/helperTypes";
+import { getGame } from "../system/util";
+import { initializeAllCharacteristics } from "../actors/context-menus/characteristic-context-menu";
 import { getCombatantsSharingToken } from "./combatant-utils";
 
 export class RqgToken extends Token {
@@ -26,6 +32,28 @@ export class RqgToken extends Token {
         const li = tracker.querySelector(`.combatant[data-combatant-id="${cb.id}"]`);
         if (li) li.classList.remove("hover");
       });
+    }
+  }
+
+  //TODO: When upgrading @league-of-foundry-developers/foundry-vtt-types
+  //we can remove the ts-nocheck at the top of this file.
+  //In the meantime this allows us to get the userId which is now the third
+  //parameter.  We need it to ensure that we don't initialize the unlinked
+  //Actor for every logged in user.
+  protected _onCreate(
+    options: PropertiesToSource<TokenDataProperties>,
+    docModOptions: DocumentModificationOptions,
+    userId: string
+  ): void {
+    super._onCreate(options, docModOptions);
+    if (userId === getGame().user?.id) {
+      //@ts-ignore actorLink
+      if (!this.document.actorLink) {
+        console.log("UNLINKED ACTOR");
+        if (this.actor) {
+          initializeAllCharacteristics(this.actor, false);
+        }
+      }
     }
   }
 }
