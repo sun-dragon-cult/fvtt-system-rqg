38e7cc81ce2917f2f20887d3d106c5a0974fc869
Åke Wivänge <ake.wivange@pagero.com>
2023-08-04 22:57:28 +0200

    refactor: organize actor sheet parts into folders

--- src/system/loadHandlebarsTemplates.ts
+++ src/system/loadHandlebarsTemplates.ts
@@ -2,40 +2,44 @@ export const loadHandlebarsTemplates = async function () {
   const templatePaths = [
     "systems/rqg/actors/rqgActorSheet.hbs",
     // ActorSheet tabs
-    "systems/rqg/actors/sheet-parts/runes-tab/runes-tab.hbs",
     "systems/rqg/actors/sheet-parts/combat-tab/combat-tab.hbs",
-    "systems/rqg/actors/sheet-parts/spirit-magic-tab.hbs",
-    "systems/rqg/actors/sheet-parts/rune-magic-tab.hbs",
-    "systems/rqg/actors/sheet-parts/sorcery-tab.hbs",
-    "systems/rqg/actors/sheet-parts/skills-tab.hbs",
-    "systems/rqg/actors/sheet-parts/gear-tab.hbs",
-    "systems/rqg/actors/sheet-parts/runes-tab/grid-rune.hbs",
-    "systems/rqg/actors/sheet-parts/grid-skill.hbs",
-    "systems/rqg/actors/sheet-parts/passions-tab.hbs",
-    "systems/rqg/actors/sheet-parts/background-tab.hbs",
-    "systems/rqg/actors/sheet-parts/activeeffects-debug-tab.hbs",
-
-    "systems/rqg/items/sheet-parts/itemActiveEffects.hbs",
-    "systems/rqg/items/sheet-parts/itemCommonPhysical.hbs",
-
-    // RqidLink parts
-    "systems/rqg/items/sheet-parts/rqidLink.hbs",
-    "systems/rqg/items/sheet-parts/rqidLinkDropzone.hbs",
-    "systems/rqg/items/sheet-parts/rqidLinkArrayDropzone.hbs",
-    "systems/rqg/items/sheet-parts/rqidLinkSelector.hbs",
-
-    // ActorSheet parts
     "systems/rqg/actors/sheet-parts/combat-tab/health/health.hbs",
     "systems/rqg/actors/sheet-parts/combat-tab/health/humanoid-hit-locations.hbs",
     "systems/rqg/actors/sheet-parts/combat-tab/health/default-hit-locations.hbs",
     "systems/rqg/actors/sheet-parts/combat-tab/health/hit-location-stats.hbs",
     "systems/rqg/actors/sheet-parts/combat-tab/combat.hbs",
     "systems/rqg/actors/sheet-parts/combat-tab/spirit-combat.hbs",
+
+    "systems/rqg/actors/sheet-parts/runes-tab/runes-tab.hbs",
     "systems/rqg/actors/sheet-parts/runes-tab/runes-elemental.hbs",
     "systems/rqg/actors/sheet-parts/runes-tab/runes-power.hbs",
     "systems/rqg/actors/sheet-parts/runes-tab/runes-form.hbs",
     "systems/rqg/actors/sheet-parts/runes-tab/runes-condition.hbs",
-    "systems/rqg/actors/sheet-parts/physical-item-location.hbs",
+    "systems/rqg/actors/sheet-parts/runes-tab/grid-rune.hbs",
+
+    "systems/rqg/actors/sheet-parts/spirit-magic-tab.hbs",
+
+    "systems/rqg/actors/sheet-parts/rune-magic-tab.hbs",
+
+    "systems/rqg/actors/sheet-parts/sorcery-tab.hbs",
+
+    "systems/rqg/actors/sheet-parts/skills-tab/skills-tab.hbs",
+    "systems/rqg/actors/sheet-parts/skills-tab/grid-skill.hbs",
+
+    "systems/rqg/actors/sheet-parts/gear-tab/gear-tab.hbs",
+    "systems/rqg/actors/sheet-parts/gear-tab/physical-item-location.hbs",
+
+    "systems/rqg/actors/sheet-parts/passions-tab.hbs",
+
+    "systems/rqg/actors/sheet-parts/background-tab.hbs",
+
+    "systems/rqg/actors/sheet-parts/activeeffects-debug-tab.hbs",
+
+    // RqidLink parts
+    "systems/rqg/items/sheet-parts/rqidLink.hbs",
+    "systems/rqg/items/sheet-parts/rqidLinkDropzone.hbs",
+    "systems/rqg/items/sheet-parts/rqidLinkArrayDropzone.hbs",
+    "systems/rqg/items/sheet-parts/rqidLinkSelector.hbs",
 
     // Actor Wizard Sheet Parts
     "systems/rqg/applications/actor-wizard-sheet-parts/creation-species.hbs",
@@ -52,6 +56,10 @@ export const loadHandlebarsTemplates = async function () {
     "systems/rqg/items/spirit-magic-item/spiritMagicSheet.hbs",
     "systems/rqg/items/rune-magic-item/runeMagicSheet.hbs",
     "systems/rqg/items/cult-item/cultSheet.hbs",
+
+    // Item sheet parts
+    "systems/rqg/items/sheet-parts/itemActiveEffects.hbs",
+    "systems/rqg/items/sheet-parts/itemCommonPhysical.hbs",
 
     // Chat Messages
     "systems/rqg/chat/characteristicChatHandler.hbs",
