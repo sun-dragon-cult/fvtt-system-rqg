2f9399d067542a34c40b4fb6b4041baddc903809
Åke Wivänge <ake@wivange.se>
2023-09-24 23:14:52 +0200

    refactor: use elvis nullcheck and combine ifs

--- src/system/damageCalculations.ts
+++ src/system/damageCalculations.ts
@@ -245,7 +245,7 @@ export class DamageCalculations {
   static getCombinedActorHealth(actor: RqgActor): ActorHealthState {
     const maxHitPoints = actor.system.attributes.hitPoints.max;
 
-    const currentMagicPoints = actor.system.attributes.magicPoints.value || 0;
+    const currentMagicPoints = actor.system.attributes.magicPoints.value ?? 0;
 
     if (maxHitPoints == null) {
       return "healthy";
@@ -260,9 +260,7 @@ export class DamageCalculations {
 
     if (totalHitPoints <= 0) {
       return "dead";
-    } else if (totalHitPoints <= 2) {
-      return "unconscious";
-    } else if (currentMagicPoints <= 0) {
+    } else if (totalHitPoints <= 2 || currentMagicPoints <= 0) {
       return "unconscious";
     } else {
       return actor.items.reduce((acc: ActorHealthState, item: RqgItem) => {
