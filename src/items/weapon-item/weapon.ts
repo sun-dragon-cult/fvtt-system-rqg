bb3cb1ca247bcc03a591a95cd728244511116d01
Åke Wivänge <ake@wivange.se>
2024-05-26 23:33:45 +0200

    fix: cannot roll damage or hit location in Version 12 closes #709

--- src/items/weapon-item/weapon.ts
+++ src/items/weapon-item/weapon.ts
@@ -400,7 +400,12 @@ export class Weapon extends AbstractEmbeddedItem {
     damageRollTerms.push(...damageBonus);
 
     const maximise = damageRollType === DamageRollTypeEnum.MaxSpecial;
-    const roll = Roll.fromTerms(damageRollTerms);
+    const damageRollTermsWithPlusOperatorsInBetween = damageRollTerms.flatMap(
+      (value, index, array) =>
+        array.length - 1 !== index ? [value, new OperatorTerm({ operator: "+" })] : value,
+    );
+
+    const roll = Roll.fromTerms(damageRollTermsWithPlusOperatorsInBetween);
     await roll.evaluate({
       maximize: maximise,
       async: true,
@@ -503,7 +508,7 @@ export class Weapon extends AbstractEmbeddedItem {
       const roll = Roll.fromTerms(specialDamage);
       await roll.evaluate({ maximize: true, async: true });
       const crushSpecialDamageTag = localize("RQG.Dialog.weaponChat.CrushSpecialDamageTag");
-      return Roll.parse(`+ ${roll.result}[${crushSpecialDamageTag}]`, {});
+      return Roll.parse(`${roll.result}[${crushSpecialDamageTag}]`, {});
     }
     return [];
   }
@@ -513,7 +518,7 @@ export class Weapon extends AbstractEmbeddedItem {
       damageType === "impale"
         ? localize("RQG.Dialog.weaponChat.ImpaleSpecialDamageTag")
         : localize("RQG.Dialog.weaponChat.SlashSpecialDamageTag");
-    return Roll.parse(`+ (${weaponDamage})[${specialDamageTag}]`, {});
+    return Roll.parse(`(${weaponDamage})[${specialDamageTag}]`, {});
   }
 
   public static getUsedSkillItem(weaponItem: RqgItem, usage: UsageType): RqgItem | undefined {
@@ -531,7 +536,7 @@ export class Weapon extends AbstractEmbeddedItem {
     }
 
     const damageBonusDamageTag = localize("RQG.Dialog.weaponChat.DamageBonusDamageTag");
-    return Roll.parse(`+${damageBonusFormula}[${damageBonusDamageTag}]`, {});
+    return Roll.parse(`${damageBonusFormula}[${damageBonusDamageTag}]`, {});
   }
 
   private static getDamageBonusString(weaponItem: RqgItem, usage: Usage): string {
