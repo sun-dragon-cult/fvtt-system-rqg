93bb5d6ed5ee09ea1cbcae505ec0c3297f6f1f7f
wake42 <ake@wivange.se>
2026-05-17 00:27:20 +0200

    chore(chat,data-model): remove deprecated duplicate combat chat data-model files

--- src/data-model/chat-data/combatChatMessage.defs.ts
+++
@@ -1,5 +0,0 @@
-// TODO there should be a DataModel for these
-
-export const attackState = ["Attacked", "Defended", "DamageRolled"] as const;
-export const usageType = ["oneHand", "twoHand", "offHand", "missile"] as const;
-export const weaponDesignation = ["none", "attackingWeapon", "parryWeapon"] as const;
